5c66249cc34a4d2d966140de606ae35176e3c5b9
Aleksandr Cilindz <aleksandr@MacBook-Air-Aleksandr.local>
2024-03-29 18:19:52 +0100

    done full version and adaptive,without burder

--- scr/js/index.js
+++ scr/js/index.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const swiper = new Swiper('.slogan-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.slogan-swiper__pagination',
+      clickable:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slogan-swiper__next',
+      prevEl: '.slogan-swiper__prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
